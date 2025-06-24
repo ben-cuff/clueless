0a5d5fbe488b142f4a2fcd7295ea25491cd82f2f
Ben Cuff <bencuff14@gmail.com>
2025-06-24 16:02:12 -0700

    give difficulty a type to fix type error

--- src/hooks/use-questions.tsx
+++ src/hooks/use-questions.tsx
@@ -98,7 +98,7 @@ export default function useQuestions() {
     setSearchInput(searchInput);
   }, []);
 
-  const handleDifficultySelectChange = useCallback((difficulty) => {
+  const handleDifficultySelectChange = useCallback((difficulty: string) => {
     setDifficulty(difficulty);
   }, []);
 
