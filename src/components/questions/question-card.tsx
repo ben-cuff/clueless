4075ea0826deef4a0d1e5fd47fa1b696279c9335
Ben Cuff <bencuff14@gmail.com>
2025-06-24 17:38:05 -0700

    standardize hashed password field and update QuestionCard component to link to leetcode

--- src/components/questions/question-card.tsx
+++ src/components/questions/question-card.tsx
@@ -2,6 +2,7 @@ import { READABLE_COMPANIES } from "@/constants/companies";
 import { READABLE_DIFFICULTIES } from "@/constants/difficulties";
 import { Question } from "@/constants/question";
 import { READABLE_TOPICS } from "@/constants/topics";
+import Link from "next/link";
 import { Badge } from "../ui/badge";
 
 export default function QuestionCard({ question }: { question: Question }) {
@@ -11,26 +12,30 @@ export default function QuestionCard({ question }: { question: Question }) {
     3: "text-red-600",
   };
 
+  const leetcodeLink = `https://leetcode.com/problems/${question.titleSlug}`;
+
   return (
-    <div className="flex flex-row items-center w-full px-4 py-2 border rounded shadow space-x-6">
-      <h2 className="text-xl font-semibold">
-        {question.questionNumber}. {question.title}
-      </h2>
-      <div className={difficultyClassMap[question.difficulty]}>
-        {READABLE_DIFFICULTIES[question.difficulty]}
+    <Link href={leetcodeLink}>
+      <div className="flex flex-row items-center w-full px-4 py-2 border rounded shadow space-x-6">
+        <h2 className="text-xl font-semibold">
+          {question.questionNumber}. {question.title}
+        </h2>
+        <div className={difficultyClassMap[question.difficulty]}>
+          {READABLE_DIFFICULTIES[question.difficulty]}
+        </div>
+        <div className="ml-5 font-bold">
+          {question.companies
+            .map((company: string) => READABLE_COMPANIES[company])
+            .join(", ")}
+        </div>
+        <div className="flex flex-wrap gap-2 ml-auto">
+          {question.topics.map((topic: string) => (
+            <Badge key={topic} className="px-2 py-1 rounded-full text-sm">
+              {READABLE_TOPICS[topic]}
+            </Badge>
+          ))}
+        </div>
       </div>
-      <div className="ml-5 font-bold">
-        {question.companies
-          .map((company: string) => READABLE_COMPANIES[company])
-          .join(", ")}
-      </div>
-      <div className="flex flex-wrap gap-2 ml-auto">
-        {question.topics.map((topic: string) => (
-          <Badge key={topic} className="px-2 py-1 rounded-full text-sm">
-            {READABLE_TOPICS[topic]}
-          </Badge>
-        ))}
-      </div>
-    </div>
+    </Link>
   );
 }
