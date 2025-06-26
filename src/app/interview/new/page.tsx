4daaab67414dda9069bfcafa8f3be7f25a64d00e
Ben Cuff <bencuff14@gmail.com>
2025-06-26 08:50:13 -0700

    add ability to choose question using a query string

--- src/app/interview/new/page.tsx
+++ src/app/interview/new/page.tsx
@@ -4,13 +4,21 @@ import { apiQuestions } from "@/utils/questionsAPI";
 import { Suspense } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-export default async function NewInterviewPage() {
+export default async function NewInterviewPage({
+  searchParams,
+}: {
+  searchParams: { questionNumber: number };
+}) {
   const numberOfQuestions = 1;
   const interviewId = uuidv4();
 
   const randomQuestionId = Math.floor(Math.random() * numberOfQuestions) + 1;
+
+  const { questionNumber } = searchParams;
+
+  const questionId = questionNumber ? questionNumber : randomQuestionId;
   const question: Question_Extended = await apiQuestions.getQuestionById(
-    randomQuestionId
+    questionId
   );
 
   return (
