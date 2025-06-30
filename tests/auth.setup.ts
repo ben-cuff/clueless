37a66b785d8152692b9bcaf449535625e850a986
Ben Cuff <bencuff14@gmail.com>
2025-06-30 09:32:55 -0700

    add basic auth test with setup and update ci

---
+++ tests/auth.setup.ts
@@ -0,0 +1,37 @@
+import { expect, test as setup } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
+
+const authFile = path.join(__dirname, "../playwright/.auth/user.json");
+
+dotenv.config();
+
+setup("authenticate", async ({ page }) => {
+  // Perform authentication steps. Replace these actions with your own.
+  await page.goto(`${process.env.BASE_URL}/login`);
+
+  await page
+    .getByRole("textbox", { name: "Username:" })
+    .fill(process.env.TEST_USERNAME || "");
+  await page
+    .getByRole("textbox", { name: "Password:" })
+    .fill(process.env.TEST_PASSWORD || "");
+  await page.getByRole("button", { name: "Sign in" }).click();
+
+  // Wait until the page receives the cookies.
+  //
+  // Sometimes login flow sets cookies in the process of several redirects.
+  // Wait for the final URL to ensure that the cookies are actually set.
+  await page.waitForURL(`${process.env.BASE_URL}/`);
+  // Alternatively, you can wait until the page reaches a state where all cookies are set.
+  await page
+    .getByRole("banner")
+    .getByRole("button")
+    .filter({ hasText: /^$/ })
+    .click();
+  await expect(page.getByRole("menuitem", { name: "Settings" })).toBeVisible();
+
+  // End of authentication steps.
+
+  await page.context().storageState({ path: authFile });
+});
