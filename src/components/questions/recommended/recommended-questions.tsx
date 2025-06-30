4b88f56707cf5ae3d741c24903113467266deac8
Ben Cuff <bencuff14@gmail.com>
2025-06-30 15:17:38 -0700

    refactor into use recommended hook

--- src/components/questions/recommended/recommended-questions.tsx
+++ src/components/questions/recommended/recommended-questions.tsx
@@ -1,39 +1,12 @@
-import { Question } from "@/types/question";
-import { apiQuestions } from "@/utils/questions-api";
-import { useSession } from "next-auth/react";
-import { useCallback, useEffect, useState } from "react";
+import useRecommended from "@/hooks/use-recommended";
 import QuestionsList from "../questions-list";
 import QuestionsLoading from "../questions-loading";
 import RecommendedHeader from "./recommended-header";
 import ToggleRecommendedButton from "./toggle-recommended-button";
 
 export default function RecommendedQuestions() {
-  const [recommendedQuestions, setRecommendedQuestions] =
-    useState<Question[]>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [isHidden, setIsHidden] = useState(false);
-  const { data: session } = useSession();
-
-  useEffect(() => {
-    (async () => {
-      if (session && session.user && session.user.id) {
-        setIsLoading(true);
-        const data = await apiQuestions.getRecommendedQuestions(
-          session.user.id
-        );
-
-        if (data) {
-          setRecommendedQuestions(data);
-        }
-        setIsLoading(false);
-      }
-    })();
-  }, [session]);
-
-  const toggleIsHidden = useCallback(() => {
-    setIsHidden(!isHidden);
-  }, [isHidden]);
-
+  const { recommendedQuestions, isLoading, isHidden, toggleIsHidden } =
+    useRecommended();
   return (
     <>
       <ToggleRecommendedButton
---
+++ src/hooks/use-recommended.tsx
@@ -0,0 +1,34 @@
+import { Question } from "@/types/question";
+import { apiQuestions } from "@/utils/questions-api";
+import { useSession } from "next-auth/react";
+import { useCallback, useEffect, useState } from "react";
+
+export default function useRecommended() {
+  const [recommendedQuestions, setRecommendedQuestions] =
+    useState<Question[]>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isHidden, setIsHidden] = useState(false);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    (async () => {
+      if (session && session.user && session.user.id) {
+        setIsLoading(true);
+        const data = await apiQuestions.getRecommendedQuestions(
+          session.user.id
+        );
+
+        if (data) {
+          setRecommendedQuestions(data);
+        }
+        setIsLoading(false);
+      }
+    })();
+  }, [session]);
+
+  const toggleIsHidden = useCallback(() => {
+    setIsHidden(!isHidden);
+  }, [isHidden]);
+
+  return { recommendedQuestions, isLoading, isHidden, toggleIsHidden };
+}
