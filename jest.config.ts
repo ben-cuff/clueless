c602469c431cba1f2c436fca140b8a8dd1ef8e10
Ben Cuff <bencuff14@gmail.com>
2025-07-01 14:47:00 -0700

    Revert "adding hooks testing temp"
    This reverts commit 921c3b2a87afddbc6d6e392056c58988c8122ca3.

--- jest.config.ts
+++ jest.config.ts
@@ -10,9 +10,6 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   testMatch: ["**/tests/unit/**/*.spec.[jt]s?(x)"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
