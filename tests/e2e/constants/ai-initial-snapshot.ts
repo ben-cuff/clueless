c05d618e2ccde4a7f1ec51f02ad8ef929f3162e4
Ben Cuff <bencuff14@gmail.com>
2025-07-02 17:45:58 -0700

    update interview tests to use constants for longer strings as well as have separate test steps

---
+++ tests/e2e/constants/ai-initial-snapshot.ts
@@ -0,0 +1,8 @@
+const AI_INITIAL_SNAPSHOT = `
+      - text: AI
+      - paragraph: Welcome to the interview! Before we begin, do you have any questions? When you're ready, please talk through your approach to the problem before you start coding. Explaining your thought process and communication skills are an important part of the interview.
+      - textbox "Your message here"
+      - button "Submit"
+      `;
+
+export default AI_INITIAL_SNAPSHOT;
