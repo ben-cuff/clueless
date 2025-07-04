737ba355bd9e9a5330676a53a398f324c867cedf
Ben Cuff <bencuff14@gmail.com>
2025-07-03 17:47:02 -0700

    implement useChatInput hook for managing chat input state and submission, get rid of use of form data due to running on server side and causing bug

--- src/components/interview/chat-input.tsx
+++ src/components/interview/chat-input.tsx
@@ -1,4 +1,7 @@
-import { useContext, useState } from "react";
+"use client";
+
+import useChatInput from "@/hooks/use-chat-input";
+import { useContext } from "react";
 import { FeedbackContext } from "../providers/feedback-provider";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
@@ -8,33 +11,31 @@ export default function ChatInput({
 }: {
   handleMessageSubmit: (message: string) => Promise<void>;
 }) {
-  const [isDisabled, setIsDisabled] = useState(false);
   const isFeedback = useContext(FeedbackContext);
 
-  const submitMessage = async (formData: FormData) => {
-    const message = formData.get("message");
-    if (!message || (typeof message === "string" && message.trim() === "")) {
-      return;
-    }
-
-    setIsDisabled(true);
-    await handleMessageSubmit(message.toString());
-    setIsDisabled(false);
-    formData.set("message", "");
-  };
+  const { handleSubmit, isDisabled, setMessage, message } =
+    useChatInput(handleMessageSubmit);
 
   return (
-    <form className="flex flex-row items-end p-2" action={submitMessage}>
+    <form
+      className="flex flex-row items-end p-2"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}
+    >
       <Textarea
         className="m-2 flex-1"
         name="message"
         placeholder="Your message here"
         rows={2}
         disabled={isDisabled || isFeedback}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       />
       <Button
         type="submit"
-        className="m-2 h-10"
+        className="m-2 h-10 hover:cursor-pointer"
         disabled={isDisabled || isFeedback}
       >
         Submit
