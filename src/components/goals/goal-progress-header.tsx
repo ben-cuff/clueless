0e8e1de2281b624424138717326f48383e227fc3
Ben Cuff <bencuff14@gmail.com>
2025-07-08 17:49:44 -0700

    fix: update goal progress header to round
    update goal progress header to round get days left so that the huge number isnt displayed

--- src/components/goals/goal-progress-header.tsx
+++ src/components/goals/goal-progress-header.tsx
@@ -14,7 +14,9 @@ export default function GoalProgressHeader({
       <CardDescription className="flex flex-col gap-1 mt-2">
         <span>Goal starts: {new Date(beginAt).toLocaleDateString()}</span>
         <span>Goal ends: {new Date(endDate).toLocaleDateString()}</span>
-        <span className="text-xl">Days left: {getDaysLeft(endDate)}</span>
+        <span className="text-xl">
+          Days left: {Math.floor(getDaysLeft(endDate))}
+        </span>
       </CardDescription>
     </CardHeader>
   );
