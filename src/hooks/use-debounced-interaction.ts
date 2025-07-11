39d20cd3d573ee612500e7feea98c63377a4c11f
Ben Cuff <bencuff14@gmail.com>
2025-07-10 18:07:50 -0700

    feat: implement useDebouncedInteraction hook for handling debounced input interactions
    created debounced interaction hook to handle saving interactions for components that I do not necessarily want to send an interation for every OnChange

---
+++ src/hooks/use-debounced-interaction.ts
@@ -0,0 +1,27 @@
+import { interactionAPI } from "@/utils/interaction-api";
+import { useCallback, useEffect } from "react";
+import useDebounce from "./use-debouncer";
+
+export default function useDebouncedInteraction(
+  value: string | number | readonly string[] | undefined,
+  interactionName?: string,
+  delay: number = 500
+) {
+  const debouncedValue = useDebounce(value, delay);
+
+  const sendInteractionData = useCallback(
+    async (value: string) => {
+      const pathname = window.location.pathname;
+      const name = interactionName ?? `textarea_change`;
+      interactionAPI.addEvent(name, pathname, value);
+    },
+    [interactionName]
+  );
+
+  useEffect(() => {
+    if (debouncedValue) {
+      sendInteractionData(debouncedValue as string);
+    }
+    console.log(debouncedValue);
+  }, [debouncedValue, sendInteractionData]);
+}
