15c3556a3862132d950de7a496abbbd228d39086
Ben Cuff <bencuff14@gmail.com>
2025-07-11 11:14:35 -0700

    feat: update clueless-interactions to version 1.0.12 and enhance GET request handling with improved filters
    this will be used to filter for things like pathname, so that I can get interactions from only the interview page.

--- src/app/api/interactions/route.ts
+++ src/app/api/interactions/route.ts
@@ -1,6 +1,7 @@
 import { cluelessInteractionsLib } from "@/lib/interactions";
 import { get200Response, get400Response } from "@/utils/api-responses";
 import { debugLog } from "@/utils/logger";
+import { Prisma } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 
@@ -28,16 +29,16 @@ export async function POST(req: Request) {
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
+  const filters: InteractionFilters = {};
+
+  // example: ?event=textarea_change+code_editor_change
+  const eventParam = url.searchParams.get("event");
+  if (eventParam) {
+    filters.event = eventParam.split(" ").map((e) => e.trim());
+  }
+
+  // example: ?userId=1
   const userId = Number(url.searchParams.get("userId"));
-
-  const filters: {
-    event?: string;
-    context?: {
-      contextField?: string;
-      contextValue?: unknown;
-    }[];
-  } = {};
-
   if (userId) {
     filters.context = [
       {
@@ -47,7 +48,42 @@ export async function GET(req: Request) {
     ];
   }
 
+  // example: ?contextField=pathname&contextValue=/interview/new&operation=string_starts_with
+  const contextField = url.searchParams.getAll("contextField");
+  const contextValue = url.searchParams.getAll("contextValue");
+  const operation = url.searchParams.getAll("operation");
+
+  if (contextField.length > 0 && contextValue.length > 0) {
+    if (contextField.length !== contextValue.length) {
+      return get400Response(
+        "contextField and contextValue must have the same length"
+      );
+    }
+
+    filters.context = [
+      ...(filters.context || []),
+      ...contextField.map((field, index) => ({
+        contextField: field,
+        contextValue: contextValue[index],
+        operation: operation[index]
+          ? { [operation[index]]: contextValue[index] }
+          : undefined,
+      })),
+    ];
+  }
+
   const interactions = await cluelessInteractionsLib.queryEvents(filters);
 
   return get200Response(interactions);
 }
+
+type ContextFilters = {
+  contextField?: string;
+  contextValue?: unknown;
+  operation?: Prisma.JsonFilter;
+};
+
+type InteractionFilters = {
+  event?: string | string[];
+  context?: ContextFilters[];
+};
