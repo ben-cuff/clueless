dd779dbd602ac5202181306d05b1b338991f8ca3
Ben Cuff <bencuff14@gmail.com>
2025-07-16 10:34:03 -0700

    feat: implement PATCH endpoint for updating user companies

---
+++ src/app/api/goal/[userId]/company/route.ts
@@ -0,0 +1,65 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { prismaLib } from "@/lib/prisma";
+import {
+  ForbiddenError,
+  get200Response,
+  get400Response,
+  UnknownServerError,
+} from "@/utils/api-responses";
+import { errorLog } from "@/utils/logger";
+import { Company, Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ userId: string }> }
+) {
+  const resolvedParams = await params;
+  const userId = Number(resolvedParams.userId);
+
+  if (isNaN(userId)) {
+    return get400Response("Invalid user ID");
+  }
+
+  const session = await getServerSession(authOptions);
+
+  if (session?.user.id !== userId) {
+    return ForbiddenError;
+  }
+
+  let companies;
+  try {
+    ({ companies } = await req.json());
+  } catch {
+    return get400Response("Invalid JSON body");
+  }
+
+  if (!Array.isArray(companies) || companies.length === 0) {
+    return get400Response("You must provide a non-empty companies array");
+  }
+
+  const validCompanies = Object.values(Company);
+
+  if (companies.some((company) => !validCompanies.includes(company))) {
+    return get400Response("One or more companies are invalid");
+  }
+
+  try {
+    const updatedGoal = await prismaLib.goal.update({
+      where: { userId },
+      data: { companies },
+    });
+
+    return get200Response(updatedGoal);
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      return get400Response("No user found for this userId");
+    } else {
+      errorLog("Unexpected error while updating user companies: " + error);
+      return UnknownServerError;
+    }
+  }
+}
