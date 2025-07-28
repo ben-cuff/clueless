b024ddbe732567123a1b3f5e29daf69be1d0b58d
Ben Cuff <bencuff14@gmail.com>
2025-07-28 09:46:19 -0700

    refactor: move formatPromptWithBreaks to separate file

--- src/components/interview/question-prompt.tsx
+++ src/components/interview/question-prompt.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import DifficultyBadge from '../difficulty-badge';
+import formatPromptWithBreaks from '../formatted-prompt';
 import { Card, CardContent } from '../ui/card';
 
 export default function QuestionPrompt({
@@ -30,24 +30,3 @@ export default function QuestionPrompt({
     </Card>
   );
 }
-
-// this function adds line breaks after certain keywords like Example, Explanation or Constraints
-// Needs to use react fragments as new lines will not persist otherwise
-//
-// Example:
-// Input: "Given an array nums, find all unique triplets. Example 1: Input: nums = [-1,0,1,2,-1,-4] Explanation: The triplets are [-1,0,1] and [-1,-1,2]. Constraints: 3 <= nums.length <= 3000"
-// Output: "Given an array nums, find all unique triplets.
-// Example 1: Input: nums = [-1,0,1,2,-1,-4]
-// Explanation: The triplets are [-1,0,1] and [-1,-1,2].
-// Constraints: 3 <= nums.length <= 3000"
-const formatPromptWithBreaks = (prompt: string) => {
-  return (prompt || 'No prompt found')
-    .replace(/(Example \d+:|Explanation:|Constraints:)/g, '\n$1')
-    .split(/\n+/)
-    .map((line, idx, arr) => (
-      <React.Fragment key={idx}>
-        {line}
-        {idx < arr.length - 1 && <br />}
-      </React.Fragment>
-    ));
-};
