84bd5c3d5a36c1d5696d5fe90d2a135c22a5e0f6
Ben Cuff <bencuff14@gmail.com>
2025-07-29 10:03:03 -0700

    feat: implement AskAI component with card, container, query form, and questions display

---
+++ src/hooks/use-ask-ai-questions.ts
@@ -0,0 +1,39 @@
+import { QuestionPartial } from '@/types/question';
+import { Nullable } from '@/types/util';
+import { handleQuestionsAPIError, QuestionsAPI } from '@/utils/questions-api';
+import { useCallback, useState } from 'react';
+
+export default function useAskAIQuestions() {
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [questions, setQuestions] = useState<Nullable<QuestionPartial[]>>(null);
+
+  const submitQuestionQuery = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await QuestionsAPI.getAskAIQuestions(message);
+      const questionData = response.questions;
+      if (
+        questionData &&
+        Array.isArray(questionData) &&
+        questionData.length > 0
+      ) {
+        setQuestions(questionData as QuestionPartial[]);
+      }
+    } catch (error) {
+      handleQuestionsAPIError(error as Error);
+    } finally {
+      setIsLoading(false);
+      setMessage('');
+    }
+  }, [message]);
+
+  return {
+    isLoading,
+    questions,
+    setQuestions,
+    message,
+    setMessage,
+    submitQuestionQuery,
+  };
+}
