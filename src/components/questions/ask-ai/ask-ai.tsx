c9a1d1d6892d72fc80f7ef8d46e3dbe7f4bff2ab
Ben Cuff <bencuff14@gmail.com>
2025-07-29 10:44:50 -0700

    feat: add ClickOutsideContainer component and useOpen hook for managing open state

--- src/components/questions/ask-ai/ask-ai.tsx
+++ src/components/questions/ask-ai/ask-ai.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import ClickOutsideContainer from '@/components/click-outside-container';
+import useOpen from '@/hooks/use-open';
 import AskAICard from './ask-ai-card';
 import AskAIContainer from './ask-ai-container';
 import CircleButton from './circle-button';
-
 export default function AskAI() {
-  const [open, setOpen] = useState(false);
+  const { open, handleClickOutsideContainer, toggleOpen } = useOpen();
 
   return (
-    <AskAIContainer>
-      <AskAICard open={open} />
-      <CircleButton
-        containerClassName="absolute -bottom-6 -right-6"
-        onClick={() => setOpen((prev) => !prev)}
-      />
-    </AskAIContainer>
+    <ClickOutsideContainer handleClickOutside={handleClickOutsideContainer}>
+      <AskAIContainer>
+        <AskAICard open={open} />
+        <CircleButton
+          containerClassName="absolute -bottom-6 -right-6"
+          onClick={toggleOpen}
+        />
+      </AskAIContainer>
+    </ClickOutsideContainer>
   );
 }
